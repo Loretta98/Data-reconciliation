e6e9c8352e87f071268ea3aa936f3aa645814d42
Loretta Salano <etta.salano@gmail.com>
2024-07-29 18:32:42 +0200

    Outliers Detection - update
    Mean or max distance for the KNN and LOF implemented (doesn't work super well)

--- Ouliers_detection_4.py
+++ Ouliers_detection_4.py
@@ -1,9 +1,12 @@
-################### Outliers detection through KNN method ###################
+################### Outliers detection through LOF method ###################
+#The Local Outlier Factor (LOF) algorithm is a more advanced method that measures the local density deviation of a given data 
+# point with respect to its neighbors. Points with a lower density than their neighbors are considered outliers.
+
 
 import numpy as np
 import pandas as pd
 import glob
-from sklearn.neighbors import NearestNeighbors, KNeighborsClassifier
+from sklearn.neighbors import NearestNeighbors, LocalOutlierFactor
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 import os 
@@ -11,7 +14,7 @@ import os
 
 # List CSV files and sort them
 path = 'C:/Users/lsalano/OneDrive - Politecnico di Milano/Desktop/FAT/Riconciliazione dati/PLC/Maggio 2024/31 Maggio 2024/Ordered CSV/Mass Reconciliation'
-outliers_st_dev_path = os.path.join(path, 'KNN')
+outliers_st_dev_path = os.path.join(path, 'LOF')
 # Create the 'outliers_st_dev' directory if it does not exist
 os.makedirs(outliers_st_dev_path, exist_ok=True)
 files = sorted(glob.glob(path + '/*.csv'))
@@ -24,27 +27,16 @@ for filename in files:
     # Extract the third column
     third_column = df.iloc[:, 2]
     data = third_column.to_frame()
-    # Assuming 'data' is a DataFrame with your features
-    scaler = StandardScaler()
-    data_scaled = scaler.fit_transform(data)
-    knn = NearestNeighbors(n_neighbors=15)  # You can choose the number of neighbors
-    knn.fit(data_scaled)
-    distances, indices = knn.kneighbors(data_scaled)
-
-    # The method to weight the distance for the KNN can be based on the mean or max distance
-
-    # # Using the mean distance to the k-nearest neighbors
-    # mean_distances = distances.mean(axis=1)
-    
-    # Using the maximum distance to the k-nearest neighbors
-    max_distances = distances.max(axis=1)
-
-    distance = max_distances
-    # Set a threshold for what you consider an outlier
-    threshold = np.mean(distance) + 2 * np.std(distance)
+    # # Assuming 'data' is a DataFrame with your features
+    # scaler = StandardScaler()
+    # data_scaled = scaler.fit_transform(data)
+    # Fit the LOF model
+    lof = LocalOutlierFactor(n_neighbors=15)  # You can choose the number of neighbors
+    outlier_scores = lof.fit_predict(data)
 
     # Identify outliers
-    outliers = distance > threshold
+    outliers = outlier_scores == -1
+    
     outlier_indices = np.where(outliers)[0]
     outliers_report_path = os.path.join(outliers_st_dev_path, f"outliers_report_{file_basename}.txt")
     with open(outliers_report_path, 'w') as report_file:
