dd5af3ac44b4b18aece12781e9846dd48a20da4a
Loretta Salano <etta.salano@gmail.com>
2024-08-02 10:49:35 +0200

    KNN correction
    The threshold is now 3*error allowed by the measurement device

--- Outliers Detection/Outliers_dection_2.py
+++ Outliers Detection/Outliers_dection_2.py
@@ -8,11 +8,11 @@ import os
 
 # List CSV files and sort them
 path = 'C:/Users/lsalano/OneDrive - Politecnico di Milano/Desktop/FAT/Riconciliazione dati/PLC/Maggio 2024/31 Maggio 2024/Ordered CSV/Mass Reconciliation'
-outliers_st_dev_path = os.path.join(path, 'KNN + ss')
+outliers_st_dev_path = os.path.join(path, 'KNN')
 # Create the 'outliers_st_dev' directory if it does not exist
 os.makedirs(outliers_st_dev_path, exist_ok=True)
 files = sorted(glob.glob(path + '/*.csv'))
-window_size = 10
+window_size = np.array([100,300,300,300,300])
 values = [3.2, 6.6, 2, 1, 0.5]
 k = 0
 
@@ -30,42 +30,26 @@ for filename in files:
     # Assuming 'data' is a DataFrame with your features
     scaler = StandardScaler()
     data_scaled = scaler.fit_transform(data)
-    knn = NearestNeighbors(n_neighbors=15)  # You can choose the number of neighbors
+    knn = NearestNeighbors(n_neighbors=window_size[k])  # You can choose the number of neighbors
     knn.fit(data_scaled)
     distances, indices = knn.kneighbors(data_scaled)
 
-    # Identify Steady states, which can be simplified as clusters with minimal variations.
-    # Option 1: to use density-based clustering (DBSCAN) to identify clusters
+    # Calculate rolling standard deviation
+    #rolling_std = third_column.rolling(window=window_size).std()
+
     # Computing the mean and standard deviation of the distances for each point's neighbors
     # Compute the mean distance
     mean_distances = distances.mean(axis=1)
-
-    # Calculate rolling standard deviation
-    rolling_std = third_column.rolling(window=window_size).std()
-    num_intervals1 = len(third_column) // window_size
-    num_intervals = np.size(rolling_std)
-    # Initialize the allowable range matrix
-    allowable_range = np.ones((len(values), num_intervals+1))
-    # Adjust allowable range based on the mean of each segment
-    for i in range(0, num_intervals1):
-        segment_mean = np.mean(third_column[i * window_size:(i + 1) * window_size])
-        for j in range(0, window_size):
-            if k == 0:
-                allowable_range[k, j + i] = values[k] * segment_mean / 100
-            elif k == 1:
-                allowable_range[k, j + i] = values[k] * segment_mean / 100
-            else:
-                allowable_range[k, j + i] = values[k]
-    # Identify steady states where rolling standard deviation is below the threshold
-    df['steady_state'] = (rolling_std < allowable_range[k, :-1]).astype(int)
-
     # Using the maximum distance to the k-nearest neighbors
     max_distances = distances.max(axis=1)
     distance = max_distances
-
+    if k == 0 & k == 1 : 
+        allowable_range = third_column*values[k]/100
+    else: 
+        allowable_range = np.ones(len(third_column))*values[k]/100
     # Set a threshold for what you consider an outlier
-    threshold = np.mean(distance) + 2 * np.std(distance)
-
+    #threshold = np.mean(distance) + 2 * np.std(distance)
+    threshold = 3*allowable_range
     # Identify outliers
     outliers = distance > threshold
     outlier_indices = np.where(outliers)[0]
@@ -74,8 +58,7 @@ for filename in files:
     results.append({
         'Filename': file_basename,
         'Number of Outliers': len(outlier_indices),
-        'Outlier Indices': outlier_indices
-    })
+        'Outlier Indices': outlier_indices})
 
     print(f"Number of outliers detected: {len(outlier_indices)}")
 
@@ -83,7 +66,7 @@ for filename in files:
     plt.figure(figsize=(24, 10))
     plt.scatter(data.index, data.iloc[:, 0], label='Data', color='blue')
     plt.scatter(data.index[outlier_indices], data.iloc[outlier_indices, 0], color='red', label='Outliers')
-    plt.title(f"Data, Outliers and steady states in {file_basename}")
+    plt.title(f"Data and Outliers in {file_basename}")
     plt.xlabel('Index')
     plt.ylabel('Value')
     plt.legend()
@@ -91,17 +74,18 @@ for filename in files:
     plot_path = os.path.join(outliers_st_dev_path, f"plot_outliers_st_dev_{file_basename}.png")
     plt.savefig(plot_path)
     plt.close()
-
+    
     plt.figure(figsize=(24, 10))
-    plt.scatter(third_column.index, third_column, label='Data points')
-    plt.scatter(third_column.index[df['steady_state'] == 1], third_column[df['steady_state'] == 1], color='green', label='Steady States')
-    plt.xlabel('Timeframe')
-    plt.ylabel('Value')
-    plt.title(f'Steady States for {file_basename}')
+    plt.plot(data.index, allowable_range,label='allowable')
+    plt.plot(data.index,threshold,label='threshold')
+    plt.plot(data.index, distance, label = 'distance')
+    plt.title(f"Allowable for {file_basename}")
     plt.legend()
-    plt.savefig(os.path.join(outliers_st_dev_path, f"{file_basename}steady_states_plot.png"))
-    plt.close()
-
+    # Save the plot
+    plot_path = os.path.join(outliers_st_dev_path, f"plot_distance_threshold{file_basename}.png")
+    plt.savefig(plot_path) 
+    
+    k = k+1
 # Convert results list to DataFrame
 results_df = pd.DataFrame(results)
 
